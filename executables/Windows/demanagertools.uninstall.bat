1c0cbda31f009be70205315919932b54c95425cf
franciscomvargas <franciscomvargas24@gmail.com>
2023-08-27 00:01:22 +0100

    Log Close App While Uninstall

--- executables/Windows/demanagertools.uninstall.bat
+++ executables/Windows/demanagertools.uninstall.bat
@@ -3,6 +3,8 @@
 :: - Model Path
 set manager_path_un=%UserProfile%\Desota\DeManagerTools
 
+
+
 :: -- Edit bellow if you're felling lucky ;) -- https://youtu.be/5NV6Rdv1a3I
 
 :: IPUT ARGS - /Q=Quietly
@@ -15,10 +17,10 @@ set sucess=
 set fail=
 set ansi_end=
 for /f "tokens=4-5 delims=. " %%i in ('ver') do set VERSION=%%i.%%j
-if "%version%" == "10.0" GOTO set_ansi_colors
-if "%version%" == "11.0" GOTO set_ansi_colors
-GOTO end_ansi_colors
-:set_ansi_colors
+if "%version%" == "10.0" GOTO set_ansi_colors_un
+if "%version%" == "11.0" GOTO set_ansi_colors_un
+GOTO end_ansi_colors_un
+:set_ansi_colors_un
 for /F %%a in ('echo prompt $E ^| cmd') do (
   set "ESC=%%a"
 )
@@ -28,16 +30,20 @@ set info_h2=%ESC%[33m
 set sucess=%ESC%[7;32m
 set fail=%ESC%[7;31m
 set ansi_end=%ESC%[0m
-:end_ansi_colors
+:end_ansi_colors_un
+
 
 :: Check if APP is running
-IF EXIST %manager_path_run%\status.txt (
-	set /p manager_status= < %manager_path_run%\status.txt
-    IF %manager_status% EQU 1 (
-        ECHO %fail%Re-Instalation Fail - Close ` Desota - Manager Tools.exe ` before attempting to Uninstall!%ansi_end%
-        GOTO EOF_MAN_UN
-    )
+IF EXIST %manager_path_un%\status.txt GOTO get_status_4_uninstall
+GOTO start_manager_uninstall
+:get_status_4_uninstall
+set /p manager_status= < %manager_path_un%\status.txt
+IF "%manager_status%" EQU "1" (
+    ECHO %fail%Re-Instalation Fail - Close ` Desota - Manager Tools.exe ` before attempting to Re/Un-Install!!%ansi_end%
+    call timeout 10
+    GOTO EOF_MAN_UN
 )
+:start_manager_uninstall
 
 :: Delete APP ShortCut
 IF EXIST "%HOMEDRIVE%%HOMEPATH%\Desktop\Desota - Manager Tools.lnk" (
@@ -56,7 +62,7 @@ IF %1 EQU /Q (
 :noargs
 :: Delete Project Folder
 IF EXIST %manager_path_un% (
-    rmdir /S %manager_path_un%  > NUL 2>NUL
+    rmdir /S %manager_path_un%
     GOTO EOF_MAN_UN
 )
 
