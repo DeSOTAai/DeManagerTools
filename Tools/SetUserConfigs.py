76d410936d941041712035dd9c3e3cae2f105b3e
franciscomvargas <franciscomvargas24@gmail.com>
2023-08-27 17:51:42 +0100

    Models Installer - Upgrade User Configs: Models

--- Tools/SetUserConfigs.py
+++ Tools/SetUserConfigs.py
@@ -1,4 +1,5 @@
 import os
+import json
 import yaml
 from yaml.loader import SafeLoader
 import argparse
@@ -20,7 +21,27 @@ with open(os.path.join(config_folder, "user.config.yaml")) as f:
 def main(args):
     user_config = USER_CONF
 
-    user_config[args.key] = args.value
+    # param "models" consist of the name of the model as key and is version as value
+    if args.key == "models":
+        # Convert models dict in cmd request to dict
+        _new_models = json.loads(args.value)
+        # Get allready installed models
+        _old_models = user_config["models"]
+        if not _old_models:
+            _old_models = {}
+        # Instatiate result models
+        _res_models = dict(_old_models)
+        for _model, _version in _new_models.items():
+            if _model in _res_models:
+                if _res_models[_model] == _version:
+                    continue
+                _res_models[_model] = _version
+                continue
+            _res_models.update({_model:_version})
+        # Update user_config `models`
+        user_config[args.key] = _res_models
+    else:
+        user_config[args.key] = args.value
 
     with open(os.path.join(config_folder, "user.config.yaml"), 'w',) as fw:
         yaml.dump(user_config,fw,sort_keys=False)
--- Tools/win_bat_manager.py
+++ Tools/win_bat_manager.py
@@ -1,20 +1,19 @@
 import os
-
+import json
 user_path=os.path.expanduser('~')
 desota_root_path=os.path.join(user_path, "Desota")
 config_folder=os.path.join(desota_root_path, "Configs")  # User | Services
 app_path=os.path.join(desota_root_path, "DeManagerTools")
 out_bat_folder=os.path.join(app_path, "executables", "Windows")
 
+# retieved from https://stackoverflow.com/a/11995662  && https://stackoverflow.com/a/10052222
 GET_ADMIN = [
-    ":: retireved from https://stackoverflow.com/a/11995662\n",
     "net session >NUL 2>NUL\n",
     "IF %errorLevel% NEQ 0 (\n",
     "\tgoto UACPrompt\n",
     ") ELSE (\n",
     "\tgoto gotAdmin\n",
     ")\n",
-    ":: retrieved from https://stackoverflow.com/a/10052222\n",
     ":UACPrompt\n",
     'ECHO Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"\n',
     "set params= %*\n"
@@ -32,31 +31,46 @@ class BatManager:
         self.system = "win"
         self.get_admin = GET_ADMIN
     def create_models_instalation(self, services_conf, models_list, target_bat_path):
+        # 1 - Get Admin Previleges 
         _tmp_file_lines = [
             "@ECHO OFF\n",
             "cls\n"
         ]
-        # Concat Admin Required Script
         _tmp_file_lines += self.get_admin
-
+        # 2 - Iterate thru instalation models
         for model in models_list:
+            # 2.1 - Append Models Installer
             _model_params = services_conf['services_params'][model]
             _installer_url = _model_params[self.system]['installer']
             _installer_args = _model_params[self.system]['installer_args']
+            _model_version = _model_params[self.system]['version']
             _installer_name = _installer_url.split('/')[-1]
             _tmp_file_lines.append(f'powershell -command "Invoke-WebRequest -Uri {_installer_url} -OutFile ~\{_installer_name}" && start /B /WAIT %UserProfile%\{_installer_name} {" ".join(_installer_args)} && del %UserProfile%\{_installer_name}\n')
-        _tmp_file_lines.append("exit\n")
+            # 2.2 - Update user models
+            _new_model = json.dumps({
+                model: _model_version
+            }).replace(" ", "").replace('"', '\\"')
+            _tmp_file_lines.append(f'call {app_path}\env\python {app_path}\Tools\SetUserConfigs.py --key models --value "{_new_model}"  > NUL 2>NUL\n')
+            # env\python Tools\SetUserConfigs.py -k models -v "{\"test\":\"how are you\"}"
+        # 4 - Delete Bat at end of instalation - retrieved from https://stackoverflow.com/a/20333152
+        _tmp_file_lines.append('(goto) 2>nul & del "%~f0"\n')
+        # 5 - Create Installer Bat
         with open(target_bat_path, "w") as fw:
-            # Step 3: Use writelines() to write the list of strings to the file
             fw.writelines(_tmp_file_lines)
 
     # Bat 2 Stop ALL Desota Services
-    def create_models_stopper(self):
+    def update_models_stopper(self, user_conf, services_conf, models_list, target_bat_path):
+        # 1 - Compare user_models with new installed models
         print("Stop")
 
     # Bat 2 Starter for models that run constantly
-    def create_models_starter(self):
+    def update_models_starter(self, user_conf, services_conf, models_list, target_bat_path):
         print("Start")
 
-    def create_desota_uninstaller(self):
+    def update_desota_uninstaller(self, user_conf, services_conf, models_list, target_bat_path):
+        # 1 - Stop Models
+        # 2 - Uninstall Models
+        # 3 - Uninstall DeManager
+        # 4 - Delete Config Folder & Portables Folder
+        # 5 - Delete DeSOTA root folder
         print("unsinstall")
