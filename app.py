0bf0234cd07e3eb543dc428e85d948f8f6b433ca
franciscomvargas <franciscomvargas24@gmail.com>
2023-08-29 22:09:59 +0100

    Models Dashboard - First Commit

--- app.py
+++ app.py
@@ -4,16 +4,19 @@ import json
 import PySimpleGUI as sg
 from Tools.services_manager import WinBatManager
 import subprocess
+import time
 
 DEBUG = True
-DESOTA_TOOLS_SERVICES = {    # Desc -> Service: Checkbox Disabled
+DESOTA_TOOLS_SERVICES = {    # Desc -> Service: Checkbox Disabled = REQUIRED
     "desotaai/derunner": True
 }
 EVENT_TO_METHOD = {
     "TABMOVE": "move_2_tab",
     "WEBREQUEST": "open_url",
     "selectTheme": "theme_select",
-    "startInstall": "install_models"
+    "startInstall": "install_models",
+    "tool_table": "tool_table_row_selected",
+    "model_table": "model_table_row_selected"
 }
 
 user_path=os.path.expanduser('~')
@@ -45,9 +48,11 @@ class SGui():
         self.system = self.user_config['system']
         
         #define pysimplegui theme
-        
         sg.theme(self.current_theme)
         
+        #define open tab
+        self.set_current_tab(self.tab_keys[0])
+
         #define pysimplegui fonts
         self.header_f = ("Helvetica", 12, "bold")
         self.title_f = ("Helvetica", 10, "bold")
@@ -66,7 +71,8 @@ class SGui():
                     sg.Tab('Models Dashboard', self.tab1, title_color='Red',border_width =10, background_color=None,tooltip='', element_justification= 'left', key=self.tab_keys[0]),
                     sg.Tab('Models Instalation', self.tab2,title_color='Blue',background_color=None, key=self.tab_keys[1]),
                     sg.Tab('DeSOTA API Key', self.tab3,title_color='Black',background_color=None,tooltip='', key=self.tab_keys[2])
-                ]], 
+                ]],
+                enable_events=True,
                 tab_location='topleft',
                 title_color='Gray', 
                 tab_background_color='White',
@@ -75,10 +81,15 @@ class SGui():
                 border_width=5
             )]
         ]
-                
+
+        #define dashboard selected rows
+        self.tools_selected = []
+        self.tools_click = True
+        self.models_selected = []
+        self.models_click = True
+
         #Define Window
         self.root = sg.Window("Desota - Manager Tools",self.tabgrp, icon=os.path.join(app_path, "Assets", "icon.ico"))
-
 
     ## Util Funks
     def get_user_theme(self):
@@ -103,6 +114,19 @@ class SGui():
         with open(os.path.join(app_path, "status.txt"), "w") as fw:
             fw.write(str(status))
 
+    def set_current_tab(self, current_tab):
+        self.current_tab = current_tab
+
+    def get_service_status(self, get_status_path):
+        _target_status_res = os.path.join(app_path, "tmp_status_serv.txt")
+        subprocess.call([get_status_path, _target_status_res])
+        while not os.path.isfile(_target_status_res):
+            time.sleep(.3)
+            continue
+        with open(_target_status_res, "r") as fr:
+            _status = fr.read().replace("\n", "").strip()
+        os.remove(_target_status_res)
+        return _status
 
     # TAB Constructors
     # TAB 1 - Models Dashboard
@@ -114,8 +138,73 @@ class SGui():
                 [sg.Button('Install Models', key=f"TABMOVE {self.tab_keys[1]}")]
             ]
         # Models Available
-        else:
-            return []
+        else: # Inspited in https://stackoverflow.com/a/65778327 
+            _dashboard_layout = []
+
+            _tools_data = []
+            _tool_table_header = ["Tool", "Service Status", "Description"]
+            for _k, _v in self.user_config['models'].items():
+                if _k not in self.tools_services:
+                    continue
+                if not _tools_data:
+                    _dashboard_layout.append([sg.Text('Installed Desota Tools', font=self.header_f)])
+                _tool_params = self.services_config["services_params"][_k]
+                _tool_desc = _tool_params["description"]
+                _tool_status_path = os.path.join(user_path, _tool_params[self.system]["service_path"], _tool_params[self.system]["status"])
+                _tool_status = self.get_service_status(_tool_status_path).lower()
+                _tool_source = _tool_params["source_code"]
+                _tools_data.append([_k, _tool_status, _tool_desc])
+            if _tools_data:
+                _dashboard_layout.append([sg.Table(
+                    values=_tools_data, 
+                    headings=_tool_table_header, 
+                    max_col_width=40,
+                    auto_size_columns=True,
+                    display_row_numbers=False,
+                    justification='center',
+                    num_rows=len(_tools_data),
+                    alternating_row_color='#000020',
+                    select_mode=sg.TABLE_SELECT_MODE_EXTENDED,
+                    enable_events=True,
+                    row_height=25,
+                    hide_vertical_scroll=True,
+                    key='tool_table'
+                )])
+                # _dashboard_layout.append([sg.Graph()]) #TODO
+                
+            _models_data = []
+            _model_table_header = ["AI Model", "Service Status", "Description"]
+            for _k, _v in self.user_config['models'].items():
+                if _k in self.tools_services:
+                    continue
+                if not _models_data:
+                    _dashboard_layout.append([sg.Text('Installed AI Models', font=self.header_f, pad=(0, (20,0)) if _tools_data else (0, (0,0)))])
+                _tool_params = self.services_config["services_params"][_k]
+                _tool_desc = _tool_params["description"]
+                _tool_status_path = os.path.join(user_path, _tool_params[self.system]["service_path"], _tool_params[self.system]["status"])
+                _tool_status = self.get_service_status(_tool_status_path).lower()
+                _tool_source = _tool_params["source_code"]
+                _models_data.append([_k, _tool_status, _tool_desc])
+            if _models_data:
+                _dashboard_layout.append([sg.Table(
+                    values=_models_data, 
+                    headings=_model_table_header, 
+                    max_col_width=40,
+                    auto_size_columns=True,
+                    display_row_numbers=False,
+                    justification='center',
+                    num_rows=len(_models_data),
+                    alternating_row_color='#000020',
+                    select_mode=sg.TABLE_SELECT_MODE_EXTENDED,
+                    enable_events=True,
+                    row_height=25,
+                    hide_vertical_scroll=True,
+                    key='model_table'
+                )])
+                # _dashboard_layout.append([sg.Graph()]) #TODO
+                _dashboard_layout.append([sg.Button('Take a Peek', button_color=("Green","White"), key="openSource", pad=(5, (20,0))), sg.Button('Source Code', button_color=("Blue","White"), key="openSource", pad=(5, (20,0))), sg.Button('Uninstall', button_color=("Red","White"), key="startUninstall", pad=(5, (20,0)))])
+
+            return _dashboard_layout
     
     # TAB 2 - Models Instalation
     def construct_install_tab(self):
@@ -127,7 +216,7 @@ class SGui():
                 if not _req_services_header:
                     _req_services_header = True
                     _install_layout.append([sg.Text('Desota Tools Services', font=self.title_f)])
-                _install_layout.append([sg.Checkbox(_desota_serv, default=True, disabled=_cb_disabled, key=f"SERVICE {_desota_serv}")])
+                _install_layout.append([sg.Checkbox(_desota_serv, default=_cb_disabled, disabled=_cb_disabled, key=f"SERVICE {_desota_serv}")])
 
         if _req_services_header:
             _install_layout.append([sg.HorizontalSeparator()])
@@ -157,9 +246,14 @@ class SGui():
                 _install_layout.append([sg.Text('Available Models', font=self.title_f)])
             _install_layout.append([sg.Checkbox(_k, key=f"SERVICE {_k}")])
 
-        
+        if not _install_layout:
+            return [
+                [sg.Text('You are an absolute LEGEND!', font=self.header_f)],
+                [sg.Text('You have currently installed all DeSOTA Models!', font=self.default_f)],
+                [sg.Button('Check 4 Upgrades', key="upgradeServConf")]
+            ]
         return [
-            [sg.Text('Select the services to be installed', font=self.header_f)],
+            [sg.Text('Select the services to be installed', font=self.header_f), sg.Button('Check 4 Upgrades', key="upgradeServConf0")],
             [sg.Column(_install_layout, size=(600,300), scrollable=True)],
             [sg.Button('Start Instalation', key="startInstall"), sg.ProgressBar(100, orientation='h', expand_x=True, size=(20, 20),  key='installPBAR')]
         ]
@@ -190,7 +284,7 @@ class SGui():
     # Methods
     # - Move Within Tkinter Tabs
     def move_2_tab(self, tab_name):
-        self.root[ tab_name ].select()
+        self.root[tab_name].select()
         return "-done-"
 
     # - Open URL in Browser
@@ -210,6 +304,8 @@ class SGui():
             if isinstance(_k, str) and "SERVICE " in _k and _v:
                 _models_2_install.append(_k.split(' ')[1].strip())
         print(f" [ DEBUG ] -> Models to install = {_models_2_install} ")
+        if not _models_2_install:
+            return "-ignore-"
         _ammount_models = len(_models_2_install)
         if self.system == "win":
             wbm = WinBatManager(self.user_config, self.services_config, _models_2_install)
@@ -243,8 +339,38 @@ class SGui():
                     #TODO : 
                     # if _ml_res == "-close-"
                     # if _ml_res == "-restart-"
+                    # if _ml_res == "-ignore-"
             return "-restart-"
 
+    # - Tool Table row selected
+    def tool_table_row_selected(self, values):
+        print(f" [tool_table_row_selected] -> {json.dumps(self.tools_selected, indent=2)}")
+        if self.tools_click:
+            for _new_sel in values["tool_table"]:
+                if _new_sel in self.tools_selected:
+                    self.tools_selected.remove(_new_sel)
+                else:
+                    self.tools_selected.append(_new_sel)
+            self.root['tool_table'].update(select_rows=self.tools_selected)
+            self.tools_click = False
+        else:
+            self.tools_click = True
+        return "-ignore-"
+    
+    # - Model Table row selected
+    def model_table_row_selected(self, values):
+        print(f" [model_table_row_selected] -> {json.dumps(self.models_selected, indent=2)}")
+        if self.models_click:
+            for _new_sel in values["model_table"]:
+                if _new_sel in self.models_selected:
+                    self.models_selected.remove(_new_sel)
+                else:
+                    self.models_selected.append(_new_sel)
+            self.root['model_table'].update(select_rows=self.models_selected)
+            self.models_click = False
+        else:
+            self.models_click = True
+        return "-ignore-"
 
     # Get Class Method From Event and Run Method
     def main_loop(self, ignore_event=[], timeout=None):
@@ -260,6 +386,10 @@ class SGui():
             return "-close-"
         elif _event == sg.TIMEOUT_KEY:
             return "-timeout-"
+        # TAB CHANGED
+        elif isinstance(_event, int):
+            self.current_tab = _values[_event]
+            return "-ignore-"
         #access all the values and if selected add them to a string
         print(f" [ DEBUG ] -> event = {_event}")
         print(f" [ DEBUG ] -> values = {_values}")
@@ -298,15 +428,26 @@ def main():
     if sgui.get_app_status() == "1":
         return 0
     sgui.set_app_status(1)
+    _tab_selected = False
+    _mem_open_tab = sgui.current_tab
     while True:
+        if not _tab_selected:
+            _tab_selected = True
+            sgui.main_loop(timeout=10)
+            sgui.move_2_tab(_mem_open_tab)
         _sgui_res = sgui.main_loop()
         print(f" [ DEBUG ] -> main_loop res = {_sgui_res}")
-
-        if _sgui_res == "-close-":
-            break
+        if _sgui_res == "-ignore-":
+            continue
         elif _sgui_res == "-restart-":
+            _mem_open_tab = sgui.current_tab
             sgui.root.close()
             sgui = SGui()
+            _tab_selected = False
+            continue
+
+        elif _sgui_res == "-close-":
+            break
         
 if __name__ == "__main__":
     main()
